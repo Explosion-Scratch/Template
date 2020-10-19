433f3f2a0f8f5a96655a6cbb16435371e54e8368
--Explosion-- <explosion.scratch@gmail.com>
2020-10-19 11:59:06 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  if (window.localStorage.getItem("dark_mode") === "true") {
+    darkMode();
+  }
+};
+
+function darkMode() {
+  var element = document.querySelector("*");
+  var dark = element.classList.toggle("dark-mode");
+  window.localStorage.setItem("dark_mode", dark);
+}
